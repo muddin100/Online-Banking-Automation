2140ee999fbd0d70a7b597dc90926e4c47431021
muddinn100 <muddin100@yahoo.com>
2023-11-16 18:59:09 -0700

    fisrt save

---
+++ src/main/java/com/generic/Login.java
@@ -0,0 +1,21 @@
+package com.generic;
+
+public class Login {
+	
+static int salary =5000;
+
+static String name ="Mohammad";
+
+static double price =2.75;
+	
+	public static void main(String[] args) {
+		System.out.println("hello");
+		System.out.println(salary);
+		System.out.println(name);
+		System.out.println(price);
+		
+
+		
+	}
+
+}
---
+++ src/main/java/com/generic/Payment.java
@@ -0,0 +1,13 @@
+package com.generic;
+
+public class Payment {
+	
+	
+	
+	
+	
+	public static void main(String[] args) {
+		
+	}
+
+}
